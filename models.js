762cc2463f051b6c80b33a47de82cbb9e8ae70c9
Azizi <azhoque@gmail.com>
2017-10-27 20:01:53 -0500

    no message

--- models.js
+++ models.js
@@ -13,7 +13,6 @@ const blogPostSchema = mongoose.Schema({
 blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
-
 blogPostSchema.methods.apiRepr = function() {
   return {
     id: this._id,
